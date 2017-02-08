57394662b583af018254a99ca52a407aef87aba4
Kevin Camellini <kevincamellini@gmail.com>
2017-02-08 02:54:22 -0500

    Optimized code from main.py to quotify.py
    Using a bajson file to hold data for quotes, authors, and file
    extension names. quotify.py to replace main.py

--- quotify.py
+++ quotify.py
@@ -1,20 +1,25 @@
-
+# Import some goodies
 import sys, json, random, io,os
 
+# Genereate a random number between 5 and 15
 def random_modulo():
     return random.randint(5,15)
 
+# Genereate a random quote - by a random author
 def gen_quote():
     random_author = random.randint(0,len(author)-1)
     random_quote = random.randint(0,len(bajson_quotes)-1)
     return bajson_quotes[random_quote] + " - " + author[random_author]
 
+# Write to file
 def write_file(stringy_string,namy_name):
+	#removes file path from namy_name
     os.remove(namy_name)
     text_file = open(namy_name, "w")
     text_file.write(stringy_string)
     text_file.close()
 
+# MaGiC
 def do_the_whole_shabang(filename):
     counter = 0
     modulo = random_modulo()
@@ -26,6 +31,7 @@ def do_the_whole_shabang(filename):
             full_file = full_file + smajson[filename[filename.index('.')+1:]] + " " + gen_quote() + "\n"
     write_file(full_file,filename)
 
+#Import Authors, Quotes, and File Extensions
 with open('db.json') as json_data:
     db = json.load(json_data)
     json_data.close()
@@ -34,5 +40,6 @@ author = db['authors']
 bajson_quotes = db['quotes']
 smajson = db['fileext']
 
+#Run the script on the output file
 if (len(sys.argv) > 1):
     do_the_whole_shabang(sys.argv[1])
