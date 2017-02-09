1a183d1d9477b3dab70e1429850450df5856040c
kcamcam <kevincamellini@gmail.com>
2017-02-09 13:22:02 -0500

    commit boringify
    Boringify.py removes all comments from a file. Unfortunately it removes
    all quotes and regular comments

---
+++ boringify.py
@@ -0,0 +1,27 @@
+# Import some goodies
+import sys, json,os
+
+#Import Authors, Quotes, and File Extensions
+with open('db.json') as json_data:
+    db = json.load(json_data)
+    json_data.close()
+smajson = db['fileext']
+
+# Write to file
+def write_file(stringy_string,namy_name):
+    os.remove(namy_name)
+    text_file = open(namy_name, "w")
+    text_file.write(stringy_string)
+    text_file.close()
+
+# MaGiC
+def boringify(filename):
+	full_file = ""
+	for line in open(filename):		
+		if (line[0] != smajson[filename[filename.index('.')+1:]]):
+			full_file = full_file + line
+	write_file(full_file,filename)
+
+	#Run the script on the output file
+if (len(sys.argv) > 1):
+    boringify(sys.argv[1])
